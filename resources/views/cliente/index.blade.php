569d446fb144888ca28cd67512a0a06a98043e90
Carlos Daniel <carloscandidofpss@gmail.com>
2021-09-02 18:41:52 -0300

    Atividade 02

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -1,39 +1,82 @@
 <html>
-  <body>
-    <form method="POST" action="/cliente">
+
+    <head>
+         <link rel="stylesheet" type="text/css" media="all" href="resources/css/main.css">
+         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+         <title>Clientes</title>
+    </head>
+
+    <body>
+       
+         <div id="BarraMenu">
+            <h1>Clientes</h1>
+
+            <div id="Links">
+               
+               <b><a href="">Home</a>
+               <b><a href="">Projeto</a>
+               <b><a href="">Contato</a>
+               <b><a href="">Sobre</a></b>
+
+            </div>
+          </div>
+      <form method="POST" action="/cliente">
+        @csrf
+        <input type="hidden" id="id" name="id" value="{{ $cliente->id }}" /> 
+        <div id="Cadastro">
+           
+
+           <div class="card">
+               
+               <div class="card-top">
+                
+                 <h2 class="title">Cadastro de Clientes</h2>
+                 
+               </div>
+
+               <div class="card-group">
+                   <label>Nome</label>
+                   <input type="text" id="nome" name="nome" placeholder="Digite Seu Nome aqui" value="{{ $cliente->nome }}" />
+               </div>
+
+               <div class="card-group">
+                   <label>CPF</label>
+                   <input type="text" id="cpf" name="cpf" placeholder="Digite Seu CPF aqui" value="{{ $cliente->cpf }}" />
+               </div>
+                <div class="card-group">
+                   <label>E-mail: </label>
+                   <input type="email" id="email" name="email" placeholder="Digite Seu E-mail aqui" value="{{ $cliente->email }}" />
+               </div>
+               <div class="card-group">
+                   <label>Telefone: </label>
+                   <input type="text" id="telefone" name="telefone" placeholder="Digite Seu Telefone aqui" value="{{ $cliente->telefone }}" />
+               </div>
+               <div class="card-group">
+                   <label>Data de Nascimento</label>
+                   <input type="date" id="data_nascimento" name="data_nascimento"  value="{{ $cliente->data_nascimento }}" />
+               </div>
+
+               <div class="card-groupbtn">
+                   <button type="submit">Salvar</button><br><br>
+
+               </div>
+               <div class="card-group">
+
+                  <button type="submit"><a href="/cliente">Novo</a></button>
+               </div>
+           </div>
       
-      @csrf
-      <input type="hidden" id="id" name="id" value="{{ $cliente->id }}" />
-      
-      <h1>Cadastro</h1>
-      <div>
-        <label for="nome">Nome: </label>
-        <input type="text" id="nome" name="nome" value="{{ $cliente->nome }}" />
-      </div>
-      <div>
-        <label for="cpf">CPF: </label>
-        <input type="text" id="cpf" name="cpf" value="{{ $cliente->cpf }}" />
-      </div>
-      <div>
-        <label for="email">E-mail: </label>
-        <input type="email" id="email" name="email" value="{{ $cliente->email }}" />
-      </div>
-      <div>
-        <label for="telefone">Telefone: </label>
-        <input type="text" id="telefone" name="telefone" value="{{ $cliente->telefone }}" />
-      </div>
-      <div>
-        <label for="data_nascimento">Data Nascimento: </label>
-        <input type="date" id="data_nascimento" name="data_nascimento" value="{{ $cliente->data_nascimento }}" />
-      </div>
-      <div>
-        <button type="submit">Salvar</button>
-        <a href="/cliente">Novo</a>
-      </div>
-    </form>
-    
-    <div>
-      <h1>Listagem</h1>
+       </div>
+      </form>    
+       <div id="Cadastro">
+           
+
+           <div class="card">
+               
+               <div class="card-top">
+                
+                 <div>
+                <h1>Listagem</h1>
       
       <table border="1">
         <thead>
@@ -65,5 +108,10 @@
       </table>  
       
     </div>
-  </body>
+      
+       </div>
+
+    </body>
+
+
 </html>
